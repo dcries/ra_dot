e939ec1c2e996f45c668e6845f4fb16802f4607e
dcries <dcries@iastate.edu>
2016-12-19 10:46:42 -0600

    rename data, edit file

--- merge_data.R
+++ merge_data.R
@@ -1,7 +1,10 @@
 library(foreign)
+library(spikeSlabGAM)
 
 setwd("\\\\my.files.iastate.edu\\Users\\dcries\\Desktop\\GIMS Rural Two-lane Primary")
 setwd("C:\\Users\\dcries\\github\\ra_dot\\data")
+setwd("C:\\Users\\dcries\\github\\ra_dot\\data")
+setwd("/home/danny/Documents/github/ra_dot/data")
 dot05 <- read.dbf("GIMS_2005.dbf")
 dot06 <- read.dbf("GIMS_2006.dbf")
 dot07 <- read.dbf("GIMS_2007.dbf")
@@ -12,6 +15,7 @@ dot11 <- read.dbf("GIMS_2011.dbf")
 dot12 <- read.dbf("GIMS_2012.dbf")
 dot13 <- read.dbf("GIMS_2013.dbf")
 dot14 <- read.dbf("GIMS_2014.dbf")
+road <- read.dbf("roaddata.dbf")
 crash <- read.dbf("crash_05-14.dbf")
 
 
@@ -24,4 +28,22 @@ which(names(dot07)!=names(dot08)) #7
 all.equal(names(dot07),names(dot09)) #true
 all.equal(names(dot07),names(dot10)) #1 string mismatch
 
+roadkeep <- c("AADT","COMPLETED","COUNTY","MSLink","NHS","TASLINKID","TRNNODE_F","TRNNODE_T","Miles")
+crashkeep <- c("CRASH01YR","CRASH02YR","CRASH03YR","CRASH04YR","CRASH05YR","CRASH06YR","CRASH07YR","CRASH08YR",
+               "CRASH09YR","CRASH10YR","CRASHES","FATCRASH","FATINJ","MAJCRASH","MAJINJ","MILES","MINCRASH",
+               "MININJ","PDOCRASH","POSCRASH","POSSINJ","TASLINKID","UNKCRASH","UNKINJ","VMT","VOLUME","YEAR")
 
+road <- road[,roadkeep]
+crash <- crash[,crashkeep]
+
+road$TASLINKID <- as.factor(road$TASLINKID)
+crash$TASLINKID <- as.factor(crash$TASLINKID)
+
+#is this correct?
+road <- unique(road)
+
+dotdata <- merge(crash,road,by="TASLINKID",all=TRUE)
+
+f1 <- MAJINJ ~ AADT + PDOCRASH + POSCRASH + VOLUME + YEAR + TRNNODE_F + TRNNODE_T + COUNTY
+
+m <- spikeSlabGAM(formula=f1,data=dotdata[1:3000,])
